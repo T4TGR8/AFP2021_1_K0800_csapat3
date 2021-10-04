863a65a2ba4885d6d81848d5af4b677409ded859
Z <zszoltan811@gmail.com>
2021-10-04 16:41:40 +0200

    Implemented prototypes of basic operations

---
+++ src/backend/muveletek.java
@@ -0,0 +1,26 @@
+public class muveletek {
+    //Az Összes művelet visszatérési Típusa String! oka: pl 3/0 = ERROR
+
+    //Prototipus: 2 szám összegének,különbségének,stb... kiszámítására képes.
+    public String Osszeadas(double a, double b){
+        return String.valueOf(a+b);
+    }
+    public String Kivonas(double a, double b){
+        return String.valueOf(a-b);
+    }
+    public String Szorzas(double a, double b){
+        return String.valueOf(a*b);
+    }
+    public String Osztas(double a, double b){
+        if (b==0) return "Nullával nem lehet osztani!";
+        return String.valueOf(a+b);
+    }
+    public String Hatvanyozas(double a, double b){
+        return String.valueOf(Math.pow(a,b));
+    }
+    public String NegyzetGyökvonas(double a){
+        return String.valueOf(Math.sqrt(a));
+    }
+
+
+}
