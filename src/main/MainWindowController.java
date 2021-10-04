6d9e2d30540e02b20366eebfcda6d7c3d5e986ac
Krajcsi Márton <krajcsimarci@gmail.com>
2021-10-04 19:11:58 +0200

    Created result label

--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,13 +1,25 @@
 package main;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
+    @FXML private Label result;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
 
+    }
+
+    @FXML
+    void onNumberClicked(MouseEvent event) {
+        int value = Integer.parseInt(((Pane)event.getSource()).getId().replace("btn",""));
+        lblResult.setText(Double.parseDouble(lblResult.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(lblResult.getText())*10+value));
     }
 }
